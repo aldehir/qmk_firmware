aldehir/qmk_firmware

commit 16a15c1cfcbfd0feb2c2cf1383676747e2f97d73

QMK-ify some GPIO macros (#8315)

drivers/avr/apa102.c
@@ -30,8 +30,8 @@
 void inline apa102_setleds(LED_TYPE *ledarray, uint16_t leds) { apa102_setleds_pin(ledarray, leds, _BV(RGB_DI_PIN & 0xF), _BV(RGB_CLK_PIN & 0xF)); }
 
 void static inline apa102_setleds_pin(LED_TYPE *ledarray, uint16_t leds, uint8_t pinmask_DI, uint8_t pinmask_CLK) {
-    pinMode(RGB_DI_PIN, PinDirectionOutput);
-    pinMode(RGB_CLK_PIN, PinDirectionOutput);
+    setPinOutput(RGB_DI_PIN);
+    setPinOutput(RGB_CLK_PIN);
 
     apa102_send_array((uint8_t *)ledarray, leds)
 }
@@ -90,7 +90,7 @@ void apa102_end_frame(uint16_t leds) {
 void apa102_send_byte(uint8_t byte) {
     uint8_t i;
     for (i = 0; i < 8; i++) {
-    digitalWrite(RGB_DI_PIN, !!(byte & (1 << (7-i)));
-    digitalWrite(RGB_CLK_PIN, PinLevelHigh);
+        writePin(RGB_DI_PIN, !!(byte & (1 << (7 - i))));
+        writePinHigh(RGB_CLK_PIN);
     }
 }

drivers/avr/ssd1306.h
@@ -3,7 +3,6 @@
 
 #include <stdbool.h>
 #include <stdio.h>
-#include "pincontrol.h"
 #include "config.h"
 
 enum ssd1306_cmds {

keyboards/40percentclub/nano/keymaps/drashna/keymap.c
@@ -1,7 +1,6 @@
 #include "drashna.h"
 #include "analog.h"
 #include "pointing_device.h"
-#include "pincontrol.h"
 
 #define KC_X0 LT(_FN, KC_ESC)
 

keyboards/claw44/ssd1306.h
@@ -2,7 +2,6 @@
 
 #include <stdbool.h>
 #include <stdio.h>
-#include "pincontrol.h"
 #include "action.h"
 
 enum ssd1306_cmds {

keyboards/comet46/ssd1306.h
@@ -2,7 +2,6 @@
 
 #include <stdbool.h>
 #include <stdio.h>
-#include "pincontrol.h"
 #include "action.h"
 
 enum ssd1306_cmds {

keyboards/converter/palm_usb/matrix.c
@@ -20,7 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 #include "protocol/serial.h"
 #include "timer.h"
-#include "pincontrol.h"
 
 
 /*
@@ -96,27 +95,27 @@ void pins_init(void) {
  // set pins for pullups, Rts , power &etc. 
 
     //print ("pins setup\n");
-    pinMode(VCC_PIN, PinDirectionOutput);
-    digitalWrite(VCC_PIN, PinLevelLow);
+    setPinOutput(VCC_PIN);
+    writePinLow(VCC_PIN);
 
 #if ( HANDSPRING == 0)
 
 #ifdef CY835
-    pinMode(GND_PIN, PinDirectionOutput);
-    digitalWrite(GND_PIN, PinLevelLow);
+    setPinOutput(GND_PIN);
+    writePinLow(GND_PIN);
 
-    pinMode(PULLDOWN_PIN, PinDirectionOutput);
-    digitalWrite(PULLDOWN_PIN, PinLevelLow);
+    setPinOutput(PULLDOWN_PIN);
+    writePinLow(PULLDOWN_PIN);
 #endif
 
-    pinMode(DCD_PIN, PinDirectionInput);
-    pinMode(RTS_PIN, PinDirectionInput); 
+    setPinInput(DCD_PIN);
+    setPinInput(RTS_PIN);
 #endif
 
 /* check that the other side isn't powered up. 
-    test=digitalRead(DCD_PIN);
+    test=readPin(DCD_PIN);
     xprintf("b%02X:", test);
-    test=digitalRead(RTS_PIN);
+    test=readPin(RTS_PIN);
     xprintf("%02X\n", test);
 */
  
@@ -129,20 +128,20 @@ uint8_t rts_reset(void) {
 // On boot, we keep rts as input, then switch roles here
 // on leaving sleep, we toggle the same way
 
-    firstread=digitalRead(RTS_PIN);
+    firstread=readPin(RTS_PIN);
    // printf("r%02X:", firstread);
 
-    pinMode(RTS_PIN, PinDirectionOutput);
+    setPinOutput(RTS_PIN);
 
-    if (firstread == PinLevelHigh) {
-        digitalWrite(RTS_PIN, PinLevelLow);
+    if (firstread) {
+        writePinLow(RTS_PIN);
     } 
      _delay_ms(10);
-    digitalWrite(RTS_PIN, PinLevelHigh);  
+    writePinHigh(RTS_PIN);
     
 
 /* the future is Arm 
-    if (palReadPad(RTS_PIN_IOPRT) == PinLevelLow)
+    if (!palReadPad(RTS_PIN_IOPRT))
   {
     _delay_ms(10);
     palSetPadMode(RTS_PINn_IOPORT, PinDirectionOutput_PUSHPULL);
@@ -224,9 +223,9 @@ uint8_t handspring_handshake(void) {
 }
 
 uint8_t handspring_reset(void) {
-    digitalWrite(VCC_PIN, PinLevelLow);
+    writePinLow(VCC_PIN);
     _delay_ms(5);
-    digitalWrite(VCC_PIN, PinLevelHigh);
+    writePinHigh(VCC_PIN);
 
     if ( handspring_handshake() ) {
         last_activity = timer_read();
@@ -250,7 +249,7 @@ void matrix_init(void)
 #endif
 
     print("power up\n");
-    digitalWrite(VCC_PIN, PinLevelHigh);
+    writePinHigh(VCC_PIN);
 
     // wait for DCD strobe from keyboard - it will do this 
     // up to 3 times, then the board needs the RTS toggled to try again
@@ -265,7 +264,7 @@ void matrix_init(void)
     }
 
 #else  /// Palm / HP  device with DCD
-    while( digitalRead(DCD_PIN) != PinLevelHigh ) {;} 
+    while( !readPin(DCD_PIN) ) {;} 
     print("dcd\n");
 
     rts_reset(); // at this point the keyboard should think all is well. 

keyboards/crkbd/ssd1306.h
@@ -2,7 +2,6 @@
 
 #include <stdbool.h>
 #include <stdio.h>
-#include "pincontrol.h"
 #include "action.h"
 
 enum ssd1306_cmds {

keyboards/helix/local_drivers/ssd1306.h
@@ -3,7 +3,6 @@
 
 #include <stdbool.h>
 #include <stdio.h>
-#include "pincontrol.h"
 #include "action.h"
 
 enum ssd1306_cmds {

commit ada5d043e5d2f56b983569f94e505f5cc29b430b

Rewrite APA102 support
The APA102 source was broken by commit 16a15c1cfcbfd0feb2c2cf1383676747e2f97d73 as it did not include the
quantum header. This commit addresses that, as well as other issues with
transferring bytes over the SPI interface, i.e. it was not setting the
clock pin back to low after sending a bit.

The deviation when sending the end frame is kept, but updated to the
latest from the referenced project.

Finally, these changes expose the global LED brightness parameter
of the APA102. Brightness values are configurable through
`APA102_DEFAULT_BRIGHTNESS` and `APA102_MAX_BRIGHTNESS`.

drivers/avr/apa102.c
@@ -21,76 +21,106 @@
  */
 
 #include "apa102.h"
-#include <avr/interrupt.h>
-#include <avr/io.h>
-#include <util/delay.h>
-#include "debug.h"
+#include "quantum.h"
 
-// Setleds for standard RGB
-void inline apa102_setleds(LED_TYPE *ledarray, uint16_t leds) { apa102_setleds_pin(ledarray, leds, _BV(RGB_DI_PIN & 0xF), _BV(RGB_CLK_PIN & 0xF)); }
+uint8_t apa102_led_brightness = APA102_DEFAULT_BRIGHTNESS;
 
-void static inline apa102_setleds_pin(LED_TYPE *ledarray, uint16_t leds, uint8_t pinmask_DI, uint8_t pinmask_CLK) {
+void static apa102_start_frame(void);
+void static apa102_end_frame(uint16_t num_leds);
+
+void static apa102_send_frame(uint8_t red, uint8_t green, uint8_t blue, uint8_t brightness);
+void static apa102_send_byte(uint8_t byte);
+
+void apa102_setleds(LED_TYPE *start_led, uint16_t num_leds) {
+    LED_TYPE *end = start_led + num_leds;
+
+    apa102_start_frame();
+    for (LED_TYPE *led = start_led; led < end; led++) {
+        apa102_send_frame(led->r, led->g, led->b, apa102_led_brightness);
+    }
+    apa102_end_frame(num_leds);
+}
+
+void static apa102_init(void) {
     setPinOutput(RGB_DI_PIN);
     setPinOutput(RGB_CLK_PIN);
 
-    apa102_send_array((uint8_t *)ledarray, leds)
+    writePinLow(RGB_DI_PIN);
+    writePinLow(RGB_CLK_PIN);
 }
 
-void apa102_send_array(uint8_t *data, uint16_t leds) {  // Data is struct of 3 bytes. RGB - leds is number of leds in data
-    apa102_start_frame();
-    while (leds--) {
-        apa102_send_frame(0xFF000000 | (data->b << 16) | (data->g << 8) | data->r);
-        data++;
-    }
-    apa102_end_frame(leds);
-}
-
-void apa102_send_frame(uint32_t frame) {
-    for (uint32_t i = 0xFF; i > 0;) {
-        apa102_send_byte(frame & i);
-        i = i << 8;
+void apa102_set_brightness(uint8_t brightness) {
+    if (brightness > APA102_MAX_BRIGHTNESS) {
+        apa102_led_brightness = APA102_MAX_BRIGHTNESS;
+    } else if (brightness < 0) {
+        apa102_led_brightness = 0;
+    } else {
+        apa102_led_brightness = brightness;
     }
 }
 
-void apa102_start_frame() { apa102_send_frame(0); }
+void static apa102_send_frame(uint8_t red, uint8_t green, uint8_t blue, uint8_t brightness) {
+    apa102_send_byte(0b11100000 | brightness);
+    apa102_send_byte(blue);
+    apa102_send_byte(green);
+    apa102_send_byte(red);
+}
 
-void apa102_end_frame(uint16_t leds) {
+void static apa102_start_frame(void) {
+    apa102_init();
+    apa102_send_byte(0);
+    apa102_send_byte(0);
+    apa102_send_byte(0);
+    apa102_send_byte(0);
+}
+
+void static apa102_end_frame(uint16_t num_leds) {
     // This function has been taken from: https://github.com/pololu/apa102-arduino/blob/master/APA102.h
     // and adapted. The code is MIT licensed. I think thats compatible?
-
-    // We need to send some more bytes to ensure that all the LEDs in the
-    // chain see their new color and start displaying it.
     //
     // The data stream seen by the last LED in the chain will be delayed by
     // (count - 1) clock edges, because each LED before it inverts the clock
     // line and delays the data by one clock edge.  Therefore, to make sure
     // the last LED actually receives the data we wrote, the number of extra
     // edges we send at the end of the frame must be at least (count - 1).
-    // For the APA102C, that is sufficient.
     //
-    // The SK9822 only updates after it sees 32 zero bits followed by one more
-    // rising edge.  To avoid having the update time depend on the color of
-    // the last LED, we send a dummy 0xFF byte.  (Unfortunately, this means
-    // that partial updates of the beginning of an LED strip are not possible;
-    // the LED after the last one you are trying to update will be black.)
-    // After that, to ensure that the last LED in the chain sees 32 zero bits
-    // and a rising edge, we need to send at least 65 + (count - 1) edges.  It
-    // is sufficent and simpler to just send (5 + count/16) bytes of zeros.
+    // Assuming we only want to send these edges in groups of size K, the
+    // C/C++ expression for the minimum number of groups to send is:
     //
-    // We are ignoring the specification for the end frame in the APA102/SK9822
-    // datasheets because it does not actually ensure that all the LEDs will
-    // start displaying their new colors right away.
-
-    apa102_send_byte(0xFF);
-    for (uint16_t i = 0; i < 5 + leds / 16; i++) {
+    //   ((count - 1) + (K - 1)) / K
+    //
+    // The C/C++ expression above is just (count - 1) divided by K,
+    // rounded up to the nearest whole number if there is a remainder.
+    //
+    // We set K to 16 and use the formula above as the number of frame-end
+    // bytes to transfer.  Each byte has 16 clock edges.
+    //
+    // We are ignoring the specification for the end frame in the APA102
+    // datasheet, which says to send 0xFF four times, because it does not work
+    // when you have 66 LEDs or more, and also it results in unwanted white
+    // pixels if you try to update fewer LEDs than are on your LED strip.
+    uint16_t iterations = (num_leds + 14) / 16;
+    for (uint16_t i = 0; i < iterations; i++) {
         apa102_send_byte(0);
     }
+
+    apa102_init();
 }
 
-void apa102_send_byte(uint8_t byte) {
-    uint8_t i;
-    for (i = 0; i < 8; i++) {
-        writePin(RGB_DI_PIN, !!(byte & (1 << (7 - i))));
-        writePinHigh(RGB_CLK_PIN);
-    }
+#define APA102_SEND_BIT(byte, bit)               \
+    do {                                         \
+        writePin(RGB_DI_PIN, (byte >> bit) & 1); \
+        writePinHigh(RGB_CLK_PIN);               \
+        writePinLow(RGB_CLK_PIN);                \
+    } while (0)
+
+void static apa102_send_byte(uint8_t byte) {
+    APA102_SEND_BIT(byte, 7);
+    APA102_SEND_BIT(byte, 6);
+    APA102_SEND_BIT(byte, 5);
+    APA102_SEND_BIT(byte, 4);
+    APA102_SEND_BIT(byte, 3);
+    APA102_SEND_BIT(byte, 2);
+    APA102_SEND_BIT(byte, 1);
+    APA102_SEND_BIT(byte, 0);
 }

drivers/avr/apa102.h
@@ -22,24 +22,25 @@
 
 #pragma once
 
-#include <avr/io.h>
-#include <avr/interrupt.h>
-
 #include "color.h"
+
+#ifndef APA102_DEFAULT_BRIGHTNESS
+#    define APA102_DEFAULT_BRIGHTNESS 31
+#endif
+
+#define APA102_MAX_BRIGHTNESS 31
+
+extern uint8_t apa102_let_brightness;
 
 /* User Interface
  *
  * Input:
- *         ledarray:           An array of GRB data describing the LED colors
- *         number_of_leds:     The number of LEDs to write
- *         pinmask (optional): Bitmask describing the output bin. e.g. _BV(PB0)
+ *         start_led:          An array of GRB data describing the LED colors
+ *         num_leds:           The number of LEDs to write
  *
  * The functions will perform the following actions:
  *         - Set the data-out pin as output
  *         - Send out the LED data
- *         - Wait 50�s to reset the LEDs
  */
-
-void apa102_setleds(LED_TYPE *ledarray, uint16_t number_of_leds);
-void apa102_setleds_pin(LED_TYPE *ledarray, uint16_t number_of_leds, uint8_t pinmask);
-void apa102_setleds_rgbw(LED_TYPE *ledarray, uint16_t number_of_leds);
+void apa102_setleds(LED_TYPE *start_led, uint16_t num_leds);
+void apa102_set_brightness(uint8_t brightness);