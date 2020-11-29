0c7ae17c13755630d97e8ae035f596f03bd26e87
Mikkel <2756925+Duckle29@users.noreply.github.com>
2020-11-29 20:46:51 +0100

    Change bit delays to 0.1 us (meassured at slightly over 200ns)

--- quantum/apa102.c
+++ quantum/apa102.c
@@ -110,8 +110,11 @@ void static apa102_end_frame(uint16_t num_leds) {
 #define APA102_SEND_BIT(byte, bit)               \
     do {                                         \
         writePin(RGB_DI_PIN, (byte >> bit) & 1); \
+        wait_us(0.1);                            \
         writePinHigh(RGB_CI_PIN);                \
+        wait_us(0.1);                            \
         writePinLow(RGB_CI_PIN);                 \
+        wait_us(0.1);                            \
     } while (0)
 
 void static apa102_send_byte(uint8_t byte) {
